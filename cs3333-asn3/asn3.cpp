0a895815342392d154ad4963e2ef03f7d9932412
Kevin Brightwell <kevin.brightwell2@gmail.com>
2014-11-11 18:26:19 -0500

    Still not done..

--- cs3333-asn3/asn3.cpp
+++ cs3333-asn3/asn3.cpp
@@ -65,14 +65,24 @@ void calculateI(const std::vector<cv::Mat> &images, const cv::Vec<float, width> 
 
 template <int width>
 void houghTransform(const cv::Mat &I, cv::Mat &out) {
+    CV_Assert(width % 2 == 1);
+
     const int WIDTHD2 = width / 2;
 
     cv::Mat outMat = cv::Mat::zeros(I.size(), CV_32FC3);
 
     // load an intermediate buffer with all of the points computing -I_x / I_y, -I_t / I_y
     cv::Mat uvbuff = cv::Mat::zeros(I.size(), CV_32FC3);
-    for ( int j = WIDTHD2; j < I.cols - WIDTHD2; ++j ) {
-        for (int i = WIDTHD2; i < I.rows - WIDTHD2; ++i ) {
+
+    cv::Mat binImg = cv::Mat::zeros(I.size(), CV_8U);
+
+    const float threshold = 5.;
+
+//    cout << "i = " << WIDTHD2 << " -> " << I.rows - WIDTHD2 - 1 << endl;
+//    cout << "j = " << WIDTHD2 << " -> " << I.cols - WIDTHD2 - 1 << endl;
+
+    for (int i = WIDTHD2; i < I.rows - WIDTHD2 - 1; ++i ) {
+        for ( int j = WIDTHD2; j < I.cols - WIDTHD2 - 1; ++j ) {
             const cv::Vec3f vI = I.at<cv::Vec3f>(i, j);
             const float ix = vI[0],
                   iy = vI[1],
@@ -87,13 +97,66 @@ void houghTransform(const cv::Mat &I, cv::Mat &out) {
                 uvbuff.at<cv::Vec3f>(i, j) = cv::Vec3f(0, 0, 0);
             }
 
+            if (std::abs(iy) > 0.01 && std::abs(u) > threshold && std::abs(v) > threshold) {
+                binImg.at<uchar>(i, j) = 255;
+            } else {
+                binImg.at<uchar>(i, j) = 0;
+            }
 
 //            cout << "uvbuff[" << i << ", " << j << "] = " << uvbuff.at<cv::Vec2f>(i, j) << std::endl;
         }
     }
 
-    uvbuff.copyTo(out);
+    cv::Mat uvRGB(uvbuff.size(), CV_8UC3);
+    cv::cvtColor(uvbuff / uchar(255), uvRGB, CV_HSV2RGB);
 
+    cv::Mat uvGrey(uvRGB.size(), CV_8U);
+    cv::cvtColor(uvRGB, uvGrey, CV_RGB2GRAY);
+
+    cout << uvGrey.depth() << endl;
+
+    cv::Mat uvGrey8U;
+    uvGrey.convertTo(uvGrey8U, CV_8U);
+
+    cv::Mat uvCanny;
+    cv::Canny(uvGrey8U, uvCanny, 1, 1);
+
+//    uvbuff.copyTo(out);
+
+    cv::Mat flow(uvCanny.size(), CV_32FC3);
+    for (int i = WIDTHD2; i < I.rows - WIDTHD2 - 1; ++i ) {
+        for ( int j = WIDTHD2; j < I.cols - WIDTHD2 - 1; ++j ) {
+            const cv::Rect roiRect(j - WIDTHD2, i - WIDTHD2, width, width);
+            const cv::Mat roi = binImg(roiRect);
+
+            cv::Mat lines;
+            cv::HoughLines(roi, lines, 1, CV_PI/180, width);
+
+            cv::Vec3f maxLine(0, 0, std::numeric_limits<float>::min());
+            if (lines.rows * lines.cols > 0) {
+                float maxDot = std::numeric_limits<float>::min();
+
+                for (int k = 0; k < lines.rows; ++k) {
+                    const cv::Vec2f line = lines.at<cv::Vec2f>(k);
+                    const float dot = line.dot(line);
+                    if (maxDot < dot) {
+                        maxDot = dot;
+                        maxLine = cv::Vec3f(line[1], line[0], dot);
+                    }
+                }
+            }
+
+            if (maxLine[2] > std::numeric_limits<float>::min()) {
+                // we found a value
+                flow.at<cv::Vec3f>(i, j) = maxLine;
+            } else {
+                flow.at<cv::Vec3f>(i, j) = cv::Vec3f(0, 0, 0);
+            }
+        }
+    }
+
+    // flow mat has the flow vectors
+    cv::blur(flow, out, cv::Size(3, 3));
 
 //    for ( int j = WIDTHD2; j < I.cols - WIDTHD2; ++j ) {
 //        for (int i = WIDTHD2; i < I.rows - WIDTHD2; ++i ) {
@@ -132,7 +195,7 @@ void houghTransform(const cv::Mat &I, cv::Mat &out) {
 //    outMat.copyTo(out);
 }
 
-#define PYR_COUNT 2
+#define PYR_COUNT 0
 
 int main(int argc, char** argv )
 {
@@ -147,12 +210,8 @@ int main(int argc, char** argv )
     for (int i = 0; i < 5; ++i) {
         cv::Mat m = cv::imread(argv[i + 1], CV_LOAD_IMAGE_GRAYSCALE);
 
-        cv::Mat tmp;
-        for (int j = 0; j < PYR_COUNT; ++j) {
-            cv::pyrDown(m, tmp);
-            m = tmp;
-        }
-        imgs[i] = m;
+        cv::blur(m, imgs[i], cv::Size(3,3));
+//        imgs[i] = m;
     }
 
     // confirm all sizes are the same
@@ -164,27 +223,62 @@ int main(int argc, char** argv )
     // 5 point central difference
     const cv::Vec<float, 5> deriv = cv::Vec<float, 5>(1., -8., 0., 8., -1.) / 12.;
 
+
     cv::Mat I;
     calculateI<5>(imgs, deriv, I);
 
     cv::Mat output;
     houghTransform<5>(I, output);
 
-    cv::Mat m = output.clone(), tmp;
-    for (int j = 0; j < PYR_COUNT; ++j) {
-        cv::pyrUp(m, tmp);
-        m = tmp;
-    }
-    output = m;
 
-    showImage("HSV Output", output);
+    cv::Mat mImg = imgs[2];
+    cv::Mat hsvImg = output;
+
+//    showImage("HSV Output", output);
 
 //    std::cout << "output = " << cv::format(output, "python") << std::endl;
 
-    cv::Mat rgbOutput;
-    cv::cvtColor(output, rgbOutput, CV_HSV2RGB);
+    // convert hsv
+    cv::Mat rgbOutput, greyOutput;
+    cv::cvtColor(hsvImg, rgbOutput, CV_HSV2BGR);
+    rgbOutput.convertTo(rgbOutput, CV_8U);
 
-    showImage("Output", rgbOutput);
+    cv::cvtColor(rgbOutput, greyOutput, CV_BGR2GRAY, 1);
+    greyOutput.convertTo(greyOutput, CV_8U);
+
+    // create mask and inverted mask
+    cv::Mat rgbMask, greyMask;
+    cv::threshold(rgbOutput, rgbMask, 1, 255, cv::THRESH_BINARY);
+    cv::threshold(greyOutput, greyMask, 1, 255, cv::THRESH_BINARY_INV);
+
+    showImage("mImg", mImg);
+    showImage("mask", rgbMask);
+
+    cv::Mat mNoHSVPixels = greyMask & mImg; // all hsv pixels are now 0
+
+    showImage("and", mNoHSVPixels);
+
+    // convert the mNoHSVPixels to be in BGR and float like the
+    cv::Mat mNoHSVPixelsRGB;
+    cv::cvtColor(mNoHSVPixels, mNoHSVPixelsRGB, CV_GRAY2BGR, 3);
+    showImage("maskedRGB", mNoHSVPixelsRGB);
+
+    cv::Mat mOutImg = mNoHSVPixelsRGB + (rgbOutput & rgbMask) ;
+
+    showImage("out?", mOutImg);
+
+//    cv::Mat out;
+//    cv::bitwise_or(rgbOutput, mImg, out, mask);
+//    showImage("and", cv::bitwise_and(mask, mImg));
+//    showImage("or", cv::bitwise_or(rgbOutput,  cv::bitwise_and(mask & mImg)));
+
+//    showImage("and", ~(mImg & bwMask));
+//    cv::Mat out = rgbOutputF | (mImg & bwMask);
+
+//    showImage("Output", rgbOutputF);
+
+//    showImage("Merged?", out);
+
     cv::waitKey(0);
 
 
